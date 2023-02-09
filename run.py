e9038cafd18a25782f1b406927f2e8106302e905
Nima Ghasemi Por <71447191+nimagp@users.noreply.github.com>
2023-02-09 22:23:34 +0330

    Temporary fix for startup bug

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ import os
 import threading
 if os.name == "nt":
     t1 = threading.Thread(target=os.system, args=(r"python bot\main.py",))
-    t2 = threading.Thread(target=os.system, args=(r"python bot\events.py",))
     t1.start()
-    t2.start()
 else:
-    os.system("python3 bot/main.py & python3 bot/events.py")
+    os.system("python3 bot/main.py")
